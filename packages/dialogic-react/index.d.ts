fbebff1c9b83b65e0884ad29046ca19a6555cbb9
Arthur Clemens <arthurclemens@gmail.com>
2020-04-26 00:58:38 +0200

    Prettier

--- packages/dialogic-react/index.d.ts
+++ packages/dialogic-react/index.d.ts
@@ -1,14 +1,14 @@
-import { FunctionComponent } from "react";
-import { dialog, notification, Dialogic } from "dialogic";
-import { UseDialogicState } from "./src/useDialogicState";
-import { UseRemaining } from "./src/useRemaining";
+import { FunctionComponent } from 'react';
+import { dialog, notification, Dialogic } from 'dialogic';
+import { UseDialogicState } from './src/useDialogicState';
+import { UseRemaining } from './src/useRemaining';
 
 export { dialog, notification };
 
-interface Dialog extends Dialogic.ComponentOptions{}
+interface Dialog extends Dialogic.ComponentOptions {}
 export const Dialog: FunctionComponent<Dialogic.ComponentOptions>;
 
-interface Notification extends Dialogic.ComponentOptions{}
+interface Notification extends Dialogic.ComponentOptions {}
 export const Notification: FunctionComponent<Dialogic.ComponentOptions>;
 
 export const useDialogicState: UseDialogicState;
