850a87ea2e2a3a2c58755e104be2407c68948e6c
Arthur Clemens <arthurclemens@gmail.com>
2023-03-03 10:29:50 +0100

    Cleanup build files

--- packages/dialogic-hooks/vite.config.ts
+++ packages/dialogic-hooks/vite.config.ts
@@ -1,26 +1,26 @@
-import { ModuleFormat } from "rollup";
-import filesize from "rollup-plugin-filesize";
-import { defineConfig } from "vite";
+import type { ModuleFormat } from 'rollup';
+import filesize from 'rollup-plugin-filesize';
+import { defineConfig } from 'vite';
 
 const packageName = process.env.npm_package_name!;
 
 export default defineConfig({
   plugins: [filesize()],
   build: {
-    target: "modules",
+    target: 'modules',
     minify: false,
     sourcemap: true,
     lib: {
-      entry: "./index.ts",
-      name: "dialogicHooks",
-      formats: ["es", "umd", "cjs"],
+      entry: './index.ts',
+      name: 'dialogicHooks',
+      formats: ['es', 'umd', 'cjs'],
       fileName: (format: ModuleFormat) => {
         switch (format) {
-          case "es":
+          case 'es':
             return `${packageName}.module.js`;
-          case "umd":
+          case 'umd':
             return `${packageName}.umd.js`;
-          case "cjs":
+          case 'cjs':
             return `${packageName}.cjs`;
           default:
             return packageName;
